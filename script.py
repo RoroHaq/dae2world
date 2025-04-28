0084e0fad3ab4c90bd96172eacafa4d0a2dc7f20
Safin Haque <safinhq13@gmail.com>
2025-04-28 11:47:49 -0400

    able to make model folders for specific models made

--- script.py
+++ script.py
@@ -1,15 +1,28 @@
 import argparse
+import shutil
+import os
 from pathlib import Path
 
-
+def dae2world(dae_path, model_name):
+  current_path = Path.cwd()
+  models_folder = current_path / 'models'
+  specific_model_folder = models_folder / model_name
+  meshes_folder = specific_model_folder / 'meshes'
+  
+  if not meshes_folder.exists():
+      meshes_folder.mkdir(parents=True)
+      print('Folder made!')
+  else:
+     ('Folder already created')
+  print(current_path)
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-df', dest='dae_path', required=True)
+  parser.add_argument('-n', dest='model_name', required=True)
   args = parser.parse_args()
 
-  dae_path = Path(args.dae_path)
-  print(dae_path)
+  dae2world(args.dae_path, args.model_name)
 
 
 
