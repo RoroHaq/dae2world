7b6d3a89057ceb55553be4b27cb7a40eac589848
Safin Haque <safinhq13@gmail.com>
2025-04-29 13:06:15 -0400

    able to write sdf files and move the initial directory in /home/user

--- script.py
+++ script.py
@@ -5,10 +5,11 @@ from jinja2 import Template
 from pathlib import Path
 
 config_file_template = Template(Path('config_template.xml').read_text(encoding='utf-8'))
+sdf_file_template = Template(Path('model_sdf_template.xml').read_text(encoding='utf-8')) 
 
 def dae2world(dae_path, model_name):
   current_path = Path.cwd()
-  file_name = Path(dae_path).name
+  mesh_name = Path(dae_path).name
   models_folder = current_path / 'models'
   specific_model_folder = models_folder / model_name
   meshes_folder = specific_model_folder / 'meshes'
@@ -17,13 +18,26 @@ def dae2world(dae_path, model_name):
       meshes_folder.mkdir(parents=True)
       print('Folder made!')
   else:
-     print('Folder already created')
-  file_custom_directory = meshes_folder / file_name
+      print('Folder already created')
+  print(Path.home())
+  destination_folder = Path.home() / '.gz' / 'models' / model_name
+
+  if not destination_folder.exists():
+    destination_folder.mkdir(parents=True)
+
+  shutil.copytree(specific_model_folder, destination_folder)
+  file_custom_directory = meshes_folder / mesh_name
+
   shutil.copyfile(dae_path, file_custom_directory)
 
-  sdf_file = "example.sdf"
+  # SDF File Creation
+  sdf_file_path = specific_model_folder / 'model.sdf'
+  sdf_file_render = sdf_file_template.render(dae_path_from_home='example')
+  with open(sdf_file_path, 'w', encoding='utf-8') as f:
+     f.write(sdf_file_render)
+  # Config File Creation
   config_file_path = specific_model_folder / 'model.config'
-  config_render = config_file_template.render(model_name= model_name, sdf_file=sdf_file)
+  config_render = config_file_template.render(model_name= model_name, sdf_file=Path(sdf_file_path).name)
   with open(config_file_path, 'w', encoding='utf-8') as f:
     f.write(config_render)
   print(current_path)
